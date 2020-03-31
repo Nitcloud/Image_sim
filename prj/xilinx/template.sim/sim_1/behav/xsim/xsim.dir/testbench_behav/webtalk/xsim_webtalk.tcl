059977de9c8582e34be91ade1f755b76e354e600
sterben <1579729633@qq.com>
2020-03-31 21:19:59 +0800

    2020/3/31

--- prj/xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/xsim_webtalk.tcl
+++ prj/xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Mar 31 21:11:01 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Mar 31 21:19:29 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "b707f647-c58c-433b-a1dd-56c6716d2eef" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "dfad90d6ab8f41669b65b5ab396f5d85" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "56" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "57" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "11 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "3" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6040_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1423159583 -regid "" -xml D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6064_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2000692467 -regid "" -xml D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/project/My_FPGA/TCL_project/Image_sim/prj/Xilinx/template.sim/sim_1/behav/xsim/xsim.dir/testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
